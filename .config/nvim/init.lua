2582ee4e9555c2190c0e2f1e8d251b1e403f7205
Peder Korsveien <pederpus@pm.me>
2021-08-22 01:03:17 +0200

    Update config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,12 +1,12 @@
-local cmd = vim.cmd  -- to execute Vim commands e.g. cmd('pwd')
-local fn = vim.fn    -- to call Vim functions e.g. fn.bufnr()
-local g = vim.g      -- a table to access global variables
-local opt = vim.opt  -- to set options
- 
+local cmd = vim.cmd
+local fn  = vim.fn
+local opt = vim.opt
+local g   = vim.g
+
 local function map(mode, lhs, rhs, opts)
-  local options = {noremap = true}
-  if opts then options = vim.tbl_extend('force', options, opts) end
-  vim.api.nvim_set_keymap(mode, lhs, rhs, options)
+    local options = {noremap = true}
+    if opts then options = vim.tbl_extend('force', options, opts) end
+    vim.api.nvim_set_keymap(mode, lhs, rhs, options)
 end
 
 -------------------- MAPPINGS -------------------------------
@@ -24,11 +24,13 @@ map('i', '<Tab>', 'pumvisible() ? "\\<C-n>" : "\\<Tab>"', {expr = true})
 map('n', '<leader>w', '<cmd>noh<CR>') -- Clear highlights
 
 map('n', '<C-l>', ':NV<cr>')                       -- Search notes
-map('n', '<C-h>', ':CHADopen<cr>')           -- Toggle file sidebar
+map('n', '<C-h>', ':NvimTreeOpen<cr>')           -- Toggle file sidebar
 
-map('n', '<leader><leader>', 'C-^')
+map('n', '<leader><leader>', '<C-^>')
 
-map('n', '<leader>r', ':source %<cr>')                              -- Source current file
+map('n', '<leader>r', ':make run<cr>')                              -- Source current file
+map('n', '<leader>m', ':make')                              -- Source current file
+
 map('n', '<leader>c', ":edit ~/.config/nvim/init.lua<cr>")   -- Edit config file
 map('n', '<leader>v', ":source ~/.config/nvim/init.lua<cr>") -- Source config file
 
@@ -56,80 +58,240 @@ Plug('tpope/vim-fugitive')
 Plug('tpope/vim-rhubarb')
 Plug('lewis6991/gitsigns.nvim')
 
--- -- Files, search and tags
-Plug('ludovicchabant/vim-gutentags')
-Plug('scrooloose/nerdtree', {on = 'NERDTreeToggle'})
-Plug('ms-jpq/chadtree', { branch = 'chad', ['do'] = 'python3 -m chadtree deps'})
+-- Search
 Plug('junegunn/fzf', {['do'] = vim.fn['fzf#install']})
 Plug('junegunn/fzf.vim')
 
--- -- Syntax, LSP and autocomplete
-Plug('nvim-treesitter/nvim-treesitter')
+-- File tree
+Plug('kyazdani42/nvim-web-devicons')
+Plug('kyazdani42/nvim-tree.lua')
+
+-- Buffers
+Plug('akinsho/nvim-bufferline.lua')
+
+-- LSP
 Plug('neovim/nvim-lspconfig')
-Plug('ms-jpq/coq_nvim', { branch = 'coq' })
-Plug('ms-jpq/coq.artifacts', { branch = 'artifacts' })
-Plug('L3MON4D3/LuaSnip')
+Plug('kabouzeid/nvim-lspinstall')
+Plug('ray-x/lsp_signature.nvim')
+
+-- Autocomplete
+Plug('ms-jpq/coq_nvim', {branch = 'coq'})
 
 -- -- Colors,icons and syntax
-Plug('kyazdani42/nvim-web-devicons')
-Plug('ryanoasis/vim-devicons')
+Plug('nvim-treesitter/nvim-treesitter')
 Plug('etdev/vim-hexcolor')
 Plug('morhetz/gruvbox')
 
 -- -- Notes
 Plug('alok/notational-fzf-vim')
-Plug('vim-scripts/DrawIt')
 
 -- -- Editing
 Plug('junegunn/vim-easy-align')
 Plug('tpope/vim-commentary')
 Plug('terryma/vim-multiple-cursors')
+Plug('vim-autoformat/vim-autoformat')
+Plug('jiangmiao/auto-pairs')
 
 vim.call('plug#end')
 
 require('nvim-treesitter.configs').setup {
-  ensure_installed = "maintained", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
-  highlight = {
-    enable = true, -- false will disable the whole extension
-  }
-}
-
--- File explorer
-local chadtree_settings = {
-    view = {
-        width = 30
-    },
-    theme = {
-        text_colour_set = "nerdtree_syntax_dark"
+    ensure_installed = "maintained", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+    highlight = {
+        enable = true, -- false will disable the whole extension
     }
 }
-vim.api.nvim_set_var("chadtree_settings", chadtree_settings)
 
+-- Bufferline
+require("bufferline").setup({
+    options = {
+        show_close_icon = true,
+        diagnostics = "nvim_lsp",
+        always_show_bufferline = true,
+        diagnostics_indicator = function(_, _, diagnostics_dict)
+            local s = " "
+            for e, n in pairs(diagnostics_dict) do
+                local sym = e == "error" and " "
+                or (e == "warning" and " " or "")
+                s = s .. sym .. n
+            end
+            return s
+        end,
+    },
+})
+
+-- File explorer
+local present, tree_c = pcall(require, "nvim-tree.config")
+if not present then
+    return
+end
+
+local tree_cb = tree_c.nvim_tree_callback
+
+g.nvim_tree_ignore = { ".git", "node_modules", ".cache" }
+g.nvim_tree_gitignore = 1
+g.nvim_tree_auto_open = 0
+g.nvim_tree_auto_close = 0 -- closes tree when it's the last window
+g.nvim_tree_quit_on_open = 0 -- closes tree when file's opened
+g.nvim_tree_follow = 1
+g.nvim_tree_indent_markers = 1
+g.nvim_tree_hide_dotfiles = 1
+g.nvim_tree_git_hl = 1
+g.nvim_tree_highlight_opened_files = 0
+g.nvim_tree_root_folder_modifier = table.concat({
+    ":t:gs?$?/..",
+    string.rep(" ", 1000),
+    "?:gs?^??",
+})
+g.nvim_tree_tab_open = 0
+g.nvim_tree_allow_resize = 1
+g.nvim_tree_add_trailing = 0 -- append a trailing slash to folder names
+g.nvim_tree_update_cwd = 1
+
+g.nvim_tree_icons = {
+    default = "",
+    symlink = "",
+    git = {
+        unstaged = "✗",
+        staged = "✓",
+        unmerged = "",
+        renamed = "➜",
+        untracked = "★",
+        deleted = "",
+        ignored = "◌",
+    },
+    folder = {
+        -- disable indent_markers option to get arrows working or if you want both arrows and indent then just add the arrow icons in front            ofthe default and opened folders below!
+        -- arrow_open = "",
+        -- arrow_closed = "",
+        default = "",
+        open = "",
+        empty = "", -- 
+        empty_open = "",
+        symlink = "",
+        symlink_open = "",
+    },
+}
+
+g.nvim_tree_bindings = {
+    { key = { "<CR>", "o", "<2-LeftMouse>" }, cb = tree_cb("edit") },
+    { key = { "<2-RightMouse>", "<C-]>" }, cb = tree_cb("cd") },
+    { key = "v", cb = tree_cb("vsplit") },
+    { key = "x", cb = tree_cb("split") },
+    { key = "<C-t>", cb = tree_cb("tabnew") },
+    { key = "H", cb = tree_cb("toggle_dotfiles") },
+    { key = "R", cb = tree_cb("refresh") },
+    { key = "x", cb = tree_cb("cut") },
+    { key = "gc", cb = tree_cb("copy") },
+    { key = "p", cb = tree_cb("paste") },
+    { key = "y", cb = tree_cb("copy_name") },
+    { key = "Y", cb = tree_cb("copy_path") },
+    { key = "gy", cb = tree_cb("copy_absolute_path") },
+    { key = "-", cb = tree_cb("dir_up") },
+    { key = "q", cb = tree_cb("close") },
+    { key = "?", cb = tree_cb("toggle_help") },
+}
+
+-- hide statusline when nvim tree is opened
+vim.cmd(
+[[au BufEnter,BufWinEnter,WinEnter,CmdwinEnter * if bufname('%') == "NvimTree" | set laststatus=0 | else | set laststatus=2 | endif]]
+)
+
+-- Autocomplete
+g.coq_settings = {
+    auto_start = true
+}
 
 -- LSP
 local nvim_lsp = require 'lspconfig'
-local on_attach = function(_, bufnr)
-  vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+local coq      = require 'coq'
 
-  local opts = { noremap = true, silent = true }
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gD', '<Cmd>lua vim.lsp.buf.declaration()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'K', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
-  -- vim.api.nvim_buf_set_keymap(bufnr, 'v', '<leader>ca', '<cmd>lua vim.lsp.buf.range_code_action()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>e', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '[d', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', ']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>q', '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
-  vim.cmd [[ command! Format execute 'lua vim.lsp.buf.formatting()' ]]
+require'lspinstall'.setup() -- important
+require("lsp_signature").setup()
+
+local servers = require'lspinstall'.installed_servers()
+for _, server in pairs(servers) do
+    require'lspconfig'[server].setup(coq.lsp_ensure_capabilities{})
+end
+
+vim.fn.sign_define(
+"LspDiagnosticsSignError",
+{ text = "", numhl = "LspDiagnosticsDefaultError" }
+)
+vim.fn.sign_define(
+"LspDiagnosticsSignWarning",
+{ text = "", numhl = "LspDiagnosticsDefaultWarning" }
+)
+vim.fn.sign_define(
+"LspDiagnosticsSignInformation",
+{ text = "", numhl = "LspDiagnosticsDefaultInformation" }
+)
+vim.fn.sign_define(
+"LspDiagnosticsSignHint",
+{ text = "", numhl = "LspDiagnosticsDefaultHint" }
+)
+
+-- set default prefix.
+vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
+vim.lsp.diagnostic.on_publish_diagnostics,
+{
+    -- virtual_text = false,
+    virtual_text = { prefix = "" },
+    signs = true,
+    update_in_insert = false,
+}
+)
+
+-- symbols for autocomplete
+vim.lsp.protocol.CompletionItemKind = {
+    "   (Text) ",
+    "   (Method)",
+    "   (Function)",
+    "   (Constructor)",
+    " ﴲ  (Field)",
+    "[] (Variable)",
+    "   (Class)",
+    " ﰮ  (Interface)",
+    "   (Module)",
+    " 襁 (Property)",
+    "   (Unit)",
+    "   (Value)",
+    " 練 (Enum)",
+    "   (Keyword)",
+    "   (Snippet)",
+    "   (Color)",
+    "   (File)",
+    "   (Reference)",
+    "   (Folder)",
+    "   (EnumMember)",
+    " ﲀ  (Constant)",
+    " ﳤ  (Struct)",
+    "   (Event)",
+    "   (Operator)",
+    "   (TypeParameter)",
+}
+
+local on_attach = function(_, bufnr)
+    vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+
+    local opts = { noremap = true, silent = true }
+    vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gD', '<Cmd>lua vim.lsp.buf.declaration()<CR>', opts)
+    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<C-]>', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
+    vim.api.nvim_buf_set_keymap(bufnr, 'n', 'K', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
+    vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
+    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
+    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
+    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
+    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
+    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
+    vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
+    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
+    vim.api.nvim_buf_set_keymap(bufnr, 'v', '<leader>ca', '<cmd>lua vim.lsp.buf.range_code_action()<CR>', opts)
+    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>e', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', opts)
+    vim.api.nvim_buf_set_keymap(bufnr, 'n', '[d', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
+    vim.api.nvim_buf_set_keymap(bufnr, 'n', ']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
+    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>q', '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
+    vim.cmd([[ command! Format execute 'lua vim.lsp.buf.formatting()' ]])
 end
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
@@ -138,27 +300,27 @@ capabilities.textDocument.completion.completionItem.snippetSupport = true
 -- Enable the following language servers
 local servers = { 'rust_analyzer', 'pyright', 'tsserver' }
 for _, lsp in ipairs(servers) do
-  nvim_lsp[lsp].setup {
-    on_attach = on_attach,
-    capabilities = capabilities,
-  }
+    nvim_lsp[lsp].setup {
+        on_attach = on_attach,
+        capabilities = capabilities,
+    }
 end
 
 require('gitsigns').setup {
-  signs = {
-    add = { hl = 'GitGutterAdd', text = '+' },
-    change = { hl = 'GitGutterChange', text = '~' },
-    delete = { hl = 'GitGutterDelete', text = '_' },
-    topdelete = { hl = 'GitGutterDelete', text = '‾' },
-    changedelete = { hl = 'GitGutterChange', text = '~' },
-  },
+    signs = {
+        add = { hl = 'GitGutterAdd', text = '+' },
+        change = { hl = 'GitGutterChange', text = '~' },
+        delete = { hl = 'GitGutterDelete', text = '_' },
+        topdelete = { hl = 'GitGutterDelete', text = '‾' },
+        changedelete = { hl = 'GitGutterChange', text = '~' },
+    },
 }
 
 -- Notes
 vim.cmd 'let g:nv_search_paths = ["~/Jottacloud/notes"]'
 
 -------------------- OPTIONS -------------------------------
-opt.completeopt   = {'menuone', 'noinsert', 'noselect'} -- completion options (for nvim-compe)
+opt.completeopt   = {'menuone', 'noinsert', 'noselect'} -- completion options
 opt.expandtab     = true                                -- use spaces insted of tabs
 opt.hidden        = true                                -- enable background buffers
 opt.ignorecase    = true
@@ -188,3 +350,4 @@ cmd 'colorscheme gruvbox'
 
 -------------------- COMMANDS -------------------------------
 cmd 'au TextYankPost * lua vim.highlight.on_yank {higroup="IncSearch", timeout=700}' -- highlight on yank
+cmd 'au BufWrite * :Autoformat'
