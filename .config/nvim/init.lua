ec96483cf3ad6517fb072410c3f497c121bedc5d
Peder Korsveien <pederpus@pm.me>
2021-08-31 17:48:51 +0200

    Update config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -9,43 +9,6 @@ local function map(mode, lhs, rhs, opts)
     vim.api.nvim_set_keymap(mode, lhs, rhs, options)
 end
 
--------------------- MAPPINGS -------------------------------
-vim.g.mapleader = ' '
-
-map('', '<leader>y', '"+y')       -- Copy to clipboard in normal, visual, select and operator modes
-map('i', '<C-u>', '<C-j>u<C-u>')  -- Make <C-u> undo-friendly
-map('i', '<C-w>', '<C-g>u<C-w>')  -- Make <C-w> undo-friendly
-
-map('', '<C-s>', ':sus<cr>') -- Suspend to terminal
-
-map('i', '<S-Tab>', 'pumvisible() ? "\\<C-p>" : "\\<Tab>"', {expr = true})
-map('i', '<Tab>', 'pumvisible() ? "\\<C-n>" : "\\<Tab>"', {expr = true})
-
-map('n', '<leader>w', '<cmd>noh<CR>') -- Clear highlights
-
-map('n', '<C-l>', ':NV<cr>')                       -- Search notes
-map('n', '<C-h>', ':NERDTreeToggle<cr>')           -- Toggle file sidebar
-
-map('n', '<leader><leader>', '<C-^>')
-
-map('n', '<leader>r', ':make run<cr>')                              -- Source current file
-map('n', '<leader>m', ':make')                              -- Source current file
-
-map('n', '<leader>c', ":edit ~/.config/nvim/init.lua<cr>")   -- Edit config file
-map('n', '<leader>v', ":source ~/.config/nvim/init.lua<cr>") -- Source config file
-
-map('n', ']<Space>', 'o<esc>k') -- Insert blank line underneath cursor
-map('n', '<cr>', 'o<esc>') -- Insert blank line
-map('n', '<bs>', 'ddk')    -- Remove blank line
-
--- Ignore case on save/close commands
-map('n', ':W', ':w')
-map('n', ':Q', ':q')
-map('n', ':X', ':x')
-
-map('n', '<C-f>', '<cmd>Rg<cr>')
-map('n', '<C-p>', '<cmd>Files<cr>')
-
 -------------------- PLUGINS -------------------------------
 
 local Plug = vim.fn['plug#']
@@ -98,6 +61,44 @@ Plug('jiangmiao/auto-pairs')
 
 vim.call('plug#end')
 
+-------------------- MAPPINGS -------------------------------
+vim.g.mapleader = ' '
+
+map('', '<leader>y', '"+y')       -- Copy to clipboard in normal, visual, select and operator modes
+map('i', '<C-u>', '<C-j>u<C-u>')  -- Make <C-u> undo-friendly
+map('i', '<C-w>', '<C-g>u<C-w>')  -- Make <C-w> undo-friendly
+
+map('', '<C-s>', ':sus<cr>') -- Suspend to terminal
+
+map('i', '<S-Tab>', 'pumvisible() ? "\\<C-p>" : "\\<Tab>"', {expr = true})
+map('i', '<Tab>', 'pumvisible() ? "\\<C-n>" : "\\<Tab>"', {expr = true})
+
+map('n', '<leader>w', '<cmd>noh<CR>') -- Clear highlights
+
+map('n', '<C-l>', ':NV<cr>')                       -- Search notes
+map('n', '<C-h>', ':NERDTreeToggle<cr>')           -- Toggle file sidebar
+
+map('n', '<leader><leader>', '<C-^>')
+
+map('n', '<leader>r', ':make run<cr>')                              -- Source current file
+map('n', '<leader>m', ':make')                              -- Source current file
+
+map('n', '<leader>c', ":edit ~/.config/nvim/init.lua<cr>")   -- Edit config file
+map('n', '<leader>v', ":source ~/.config/nvim/init.lua<cr>") -- Source config file
+
+map('n', ']<Space>', 'o<esc>k') -- Insert blank line underneath cursor
+map('n', '<cr>', 'o<esc>') -- Insert blank line
+map('n', '<bs>', 'ddk')    -- Remove blank line
+
+-- Ignore case on save/close commands
+map('n', ':W', ':w')
+map('n', ':Q', ':q')
+map('n', ':X', ':x')
+
+map('n', '<C-f>', '<cmd>Rg<cr>')
+map('n', '<C-p>', '<cmd>Files<cr>')
+
+
 require('nvim-treesitter.configs').setup {
     ensure_installed = "maintained", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
     highlight = {
@@ -122,78 +123,6 @@ require("bufferline").setup({
         end,
     },
 })
-
--- File explorer
-local present, tree_c = pcall(require, "nvim-tree.config")
-if not present then
-    return
-end
-
-local tree_cb = tree_c.nvim_tree_callback
-
-g.nvim_tree_ignore = { ".git", "node_modules", ".cache" }
-g.nvim_tree_gitignore = 1
-g.nvim_tree_auto_open = 0
-g.nvim_tree_auto_close = 0 -- closes tree when it's the last window
-g.nvim_tree_quit_on_open = 0 -- closes tree when file's opened
-g.nvim_tree_follow = 1
-g.nvim_tree_indent_markers = 1
-g.nvim_tree_hide_dotfiles = 1
-g.nvim_tree_git_hl = 1
-g.nvim_tree_highlight_opened_files = 0
-g.nvim_tree_root_folder_modifier = table.concat({
-    ":t:gs?$?/..",
-    string.rep(" ", 1000),
-    "?:gs?^??",
-})
-g.nvim_tree_tab_open = 0
-g.nvim_tree_allow_resize = 1
-g.nvim_tree_add_trailing = 0 -- append a trailing slash to folder names
-g.nvim_tree_update_cwd = 1
-
-g.nvim_tree_icons = {
-    default = "",
-    symlink = "",
-    git = {
-        unstaged = "✗",
-        staged = "✓",
-        unmerged = "",
-        renamed = "➜",
-        untracked = "★",
-        deleted = "",
-        ignored = "◌",
-    },
-    folder = {
-        -- disable indent_markers option to get arrows working or if you want both arrows and indent then just add the arrow icons in front            ofthe default and opened folders below!
-        -- arrow_open = "",
-        -- arrow_closed = "",
-        default = "",
-        open = "",
-        empty = "", -- 
-        empty_open = "",
-        symlink = "",
-        symlink_open = "",
-    },
-}
-
-g.nvim_tree_bindings = {
-    { key = { "<CR>", "o", "<2-LeftMouse>" }, cb = tree_cb("edit") },
-    { key = { "<2-RightMouse>", "<C-]>" }, cb = tree_cb("cd") },
-    { key = "v", cb = tree_cb("vsplit") },
-    { key = "x", cb = tree_cb("split") },
-    { key = "<C-t>", cb = tree_cb("tabnew") },
-    { key = "H", cb = tree_cb("toggle_dotfiles") },
-    { key = "R", cb = tree_cb("refresh") },
-    { key = "x", cb = tree_cb("cut") },
-    { key = "gc", cb = tree_cb("copy") },
-    { key = "p", cb = tree_cb("paste") },
-    { key = "y", cb = tree_cb("copy_name") },
-    { key = "Y", cb = tree_cb("copy_path") },
-    { key = "gy", cb = tree_cb("copy_absolute_path") },
-    { key = "-", cb = tree_cb("dir_up") },
-    { key = "q", cb = tree_cb("close") },
-    { key = "?", cb = tree_cb("toggle_help") },
-}
 
 -- Autocomplete
 g.coq_settings = {
