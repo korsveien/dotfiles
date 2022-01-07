e437b7f88fcd4d350a0807373455b7185b3e5381
pederpus <pederpus@pm.me>
2022-01-07 17:40:21 +0100

    Update config

---
+++ .config/dwm/bar.sh
@@ -0,0 +1,36 @@
+#! /usr/bin/fish
+
+function cpu
+    echo "CPU" (grep -o "^[^ ]*" /proc/loadavg)
+end
+
+function battery
+    echo " "(cat /sys/class/power_supply/BAT0/capacity)"%"
+end
+
+function brightness
+    echo " "(cat /sys/class/backlight/intel_backlight/brightness)
+end
+
+function mem
+    echo " "(free -h | awk '/^Mem/ { print $3 }' | sed s/i//g)
+end
+
+function wlan
+    switch (cat /sys/class/net/wlan0/operstate 2>/dev/null)
+        case up
+            echo "󰤨"(nmcli -f IN-USE,SIGNAL,SSID device wifi | awk '/^\*/{if (NR!=1) {print $2}}')"%"
+        case down
+            echo "󰤭 Disconnected"
+        case '*'
+            echo "ERROR: could not check wifi status"
+    end
+end
+
+function clock
+    echo (date '+%H:%M')
+end
+
+while true
+    sleep 1 && xsetroot -name (echo (mem) (brightness) (wlan) (battery) (clock))
+end
