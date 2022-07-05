a8b5c6a7f29ed52c61592a85b75f87db93f7d0eb
pederpus <pederpus@icloud.com>
2022-07-05 19:58:15 +0200

    Update config

--- .config/nvim/lua/config/terminal.lua
+++ .config/nvim/lua/config/terminal.lua
@@ -1,7 +1,7 @@
 require 'toggleterm'.setup {
     size = vim.api.nvim_win_get_width(0) * 0.4,
     shell = 'fish',
-    open_mapping = '<c-,>',
+    open_mapping = '§',
     direction = 'vertical',
     on_open = function(term)
         term.cmd = 'cd ' .. vim.fn.getcwd()
