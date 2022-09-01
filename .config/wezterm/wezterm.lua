a77d955ad1fe54bdcabf0f55248a15c52bd44078
peder <peder@korsveien.me>
2022-09-01 11:44:32 +0200

    Update config

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -6,8 +6,23 @@ return {
     font_size = 17.0,
     use_fancy_tab_bar = true,
     hide_tab_bar_if_only_one_tab = true,
+    pane_focus_follows_mouse = true,
 
     keys = {
+        {
+            key = 'd',
+            mods = 'CMD',
+            action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' }, 
+        },
+        {
+            key = 'd',
+            mods = 'CMD|SHIFT',
+            action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' }, 
+        },
+        { key = "h", mods = "CMD", action=wezterm.action{ActivatePaneDirection="Left"}},
+        { key = "j", mods = "CMD", action=wezterm.action{ActivatePaneDirection="Down"}},
+        { key = "k", mods = "CMD", action=wezterm.action{ActivatePaneDirection="Up"}},
+        { key = "l", mods = "CMD", action=wezterm.action{ActivatePaneDirection="Right"}},
     },
 
     send_composed_key_when_left_alt_is_pressed = true,
