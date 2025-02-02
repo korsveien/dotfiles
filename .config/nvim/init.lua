1e56a96903fc36da8978e1ded247d9e75e350613
Peder Korsveien <peder.korsveien@bekk.no>
2025-02-02 23:10:10 +0100

    Update config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,15 +1,5 @@
 require("config.lazy")
+require("config.keybindings")
+require("config.autocommands")
 
 vim.cmd([[colorscheme habamax]])
-
--- https://www.mukeshsharma.dev/2022/02/08/neovim-workflow-for-terraform.html
--- autoformat terraform files
-vim.cmd([[silent! autocmd! filetypedetect BufRead,BufNewFile *.tf]])
-vim.cmd([[autocmd BufRead,BufNewFile *.hcl set filetype=hcl]])
-vim.cmd([[autocmd BufRead,BufNewFile .terraformrc,terraform.rc set filetype=hcl]])
-vim.cmd([[autocmd BufRead,BufNewFile *.tf,*.tfvars set filetype=terraform]])
-vim.cmd([[autocmd BufRead,BufNewFile *.tfstate,*.tfstate.backup set filetype=json]])
-
--- automatically format *.tf and *.tfvars files with terraform fmt on save and align settings.
-vim.cmd([[let g:terraform_fmt_on_save=1]])
-vim.cmd([[let g:terraform_align=1]])
