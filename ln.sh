7ed6a3ba077ccc848133e63a242972d0607273f8
Peder Korsveien <peder@korsveien.me>
2026-02-02 19:01:51 +0100

    Add tmux config

--- ln.sh
+++ ln.sh
@@ -4,8 +4,10 @@ rm -rf ~/.config/nvim
 rm -rf ~/.config/fish
 rm -rf ~/.config/alacritty
 rm -rf ~/.config/hypr
+rm -rf ~/.config/tmux
 
 ln -s ~/dotfiles/nvim ~/.config/nvim
 ln -s ~/dotfiles/fish ~/.config/fish
 ln -s ~/dotfiles/alacritty ~/.config/alacritty
 ln -s ~/dotfiles/hypr ~/.config/hypr
+ln -s ~/dotfiles/tmux ~/.config/tmux
