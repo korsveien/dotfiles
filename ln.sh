c08e8e4286613bf6cd65e5ea933515f9b5e16a4c
Peder Korsveien <peder.korsveien@bekk.no>
2026-06-08 12:08:01 +0200

    Add zed config

--- ln.sh
+++ ln.sh
@@ -5,9 +5,11 @@ rm -rf ~/.config/fish
 rm -rf ~/.config/kitty
 rm -rf ~/.config/yazi
 rm -rf ~/.config/opencode
+rm -rf ~/.config/zed
 
 ln -s ~/dotfiles/nvim ~/.config/nvim
 ln -s ~/dotfiles/fish ~/.config/fish
 ln -s ~/dotfiles/kitty ~/.config/kitty
 ln -s ~/dotfiles/yazi ~/.config/yazi
 ln -s ~/dotfiles/opencode ~/.config/opencode
+ln -s ~/dotfiles/zed ~/.config/zed
