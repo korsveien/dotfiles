d3638844feb7fadbfdcf68b852ea169f29bb92a5
korsveien <peder.korsveien@sparebank1.no>
2026-08-20 10:35:25 +0200

    Legg tilbake config for kitty

--- ln.sh
+++ ln.sh
@@ -3,7 +3,9 @@
 rm -rf ~/.config/nvim
 rm -rf ~/.config/fish
 rm -rf ~/.config/tmux
+rm -rf ~/.config/kitty
 
 ln -s ~/dotfiles/nvim ~/.config/nvim
 ln -s ~/dotfiles/fish ~/.config/fish
 ln -s ~/dotfiles/tmux ~/.config/tmux
+ln -s ~/dotfiles/kitty ~/.config/kitty
