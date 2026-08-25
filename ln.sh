95be244af32cc9c7eca3fab56c7b8b548f9ae7f8
korsveien <peder.korsveien@sparebank1.no>
2026-08-25 08:28:27 +0200

    Add back zed config

--- ln.sh
+++ ln.sh
@@ -4,8 +4,12 @@ rm -rf ~/.config/nvim
 rm -rf ~/.config/fish
 rm -rf ~/.config/tmux
 rm -rf ~/.config/kitty
+rm -rf ~/.config/yazi
+rm -rf ~/.config/zed
 
 ln -s ~/dotfiles/nvim ~/.config/nvim
 ln -s ~/dotfiles/fish ~/.config/fish
 ln -s ~/dotfiles/tmux ~/.config/tmux
 ln -s ~/dotfiles/kitty ~/.config/kitty
+ln -s ~/dotfiles/yazi ~/.config/yazi
+ln -s ~/dotfiles/zed ~/.config/zed
